26b89ed69f49d5ba038e32c7724a07a1e1bf4e8c
Brian Reavis <brian@thirdroute.com>
2012-06-05 12:09:26 -0700

    Tabs to spaces.

--- js/dep/formdata.js
+++ js/dep/formdata.js
@@ -22,11 +22,11 @@
         this._fields.forEach(function(field) {
             body += "--" + boundary + "\r\n";
             // file upload
-			if (field[1].name || field[1].fileName) {
+            if (field[1].name || field[1].fileName) {
                 var file = field[1];
                 var filename = field[1].name || field[1].fileName;
-				var filetype = file.type || 'application/octet-stream';
-				body += "Content-Disposition: form-data; name=\""+ field[0] +"\"; filename=\""+ filename +"\"\r\n";
+                var filetype = file.type || 'application/octet-stream';
+                body += "Content-Disposition: form-data; name=\""+ field[0] +"\"; filename=\""+ filename +"\"\r\n";
                 body += "Content-Type: "+ filetype +"\r\n\r\n";
                 body += file.getAsBinary() + "\r\n";
             } else {
